686a2bfa825a3870ac66cf3109f11bb3574e5f4a
Dana <danacarroll2000@gmail.com>
2021-06-27 16:29:53 -0400

    Added active navigation classes on scroll

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-// 
+// ------------ NAV OFFSET ------------
 $("nav a[href^='#']").click(function (e) {
     // prevents browser from doing a default click
     e.preventDefault();
 
     // finds the position of selected link/ID
-    let idPos = $($(this).attr("href")).offset().top - $("header").innerHeight();
+    let idPos = $($(this).attr("href")).offset().top - $("header").innerHeight() + 5;
 
     // animates to selected section position
     $("body, html").animate({scrollTop: idPos}, 1000);
@@ -20,3 +20,30 @@ $("header a").click(function (e) {
     // animates to selected section position
     $("body, html").animate({scrollTop: idPos}, 1000);
 });
+
+$(document).ready(function() {
+    let scrollLink = $('.navItem');
+    let projectSectionTop = $('#projects').offset().top;
+
+    $(window).scroll(function() {
+        // where we are currently on the screen
+        let scrollbarLocation = $(this).scrollTop() + $('header').innerHeight() + 5;
+
+        scrollLink.each(function(){
+
+            // calculate how far from the top each section is REGULARLY
+            let sectionOffset = $(this.hash).offset().top;
+
+            // adding active class to nav items
+            if ( sectionOffset <= scrollbarLocation) {
+                $(this).addClass('active');
+                $(this).siblings().removeClass('active');
+            }
+            // if scroll is above the project section remove active class
+            if (scrollbarLocation <= projectSectionTop) {
+                $(this).removeClass('active');
+            }
+
+        })
+    })
+})
