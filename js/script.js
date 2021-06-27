d86b202840a3ad0307bd9ce23c01bbb547d89f70
Dana <danacarroll2000@gmail.com>
2021-06-27 10:21:27 -0400

    Static mobile website, no case studies

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// let nav = document.querySelector('header');
+// let navHeight = nav.clientHeight;
+// let body = document.querySelector('body');
+// let bodyWidth = body.clientWidth;
+
+// window.onload = function() {
+//     // gsap.from('header', {
+//     //     opacity: 0,
+//     //     y: -navHeight,
+//     //     duration: 0.75,
+//     //     delay: 0.5
+//     // });
+//     gsap.from('#homeText h2', {
+//         opacity: 0,
+//         x: bodyWidth,
+//         duration: 0.75,
+//         delay: 1
+//     });
+//     gsap.from('#homeText p', {
+//         opacity: 0,
+//         x: -bodyWidth,
+//         duration: 0.75,
+//         delay: 1
+//     });
+//     gsap.set('#home', {
+//         paddingTop: navHeight + 100
+//     });
+// }
